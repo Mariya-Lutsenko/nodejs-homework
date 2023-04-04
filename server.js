2e500a96b9b9537f6e7db2663d001228245b2fb4
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-04-04 21:33:32 +0300

    db-contacts

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+//Wt9utGe40IaFOtER
+
+const DB_HOST =
+  "mongodb+srv://Mariia:Wt9utGe40IaFOtER@cluster0.l3t55no.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => console.log(error.message));
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
