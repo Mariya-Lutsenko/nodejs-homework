6140ed7e4ad90a1141bfc6c73542a6a83f1a869a
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-04-12 13:07:45 +0300

    token by user

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -16,11 +16,15 @@ const login = async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
   //   await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
     token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
   });
 };
 module.exports = login;
